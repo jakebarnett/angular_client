abbdb5974b8b31e2eaac2dc00e9d6480b425d984
Jacob Barnett <mrjbarnett@gmail.com>
2015-03-11 00:19:46 -0700

    pull services out of unicornController

--- app/js/client.js
+++ app/js/client.js
@@ -4,4 +4,11 @@ require('angular/angular');
 
 var UnicornsApp = angular.module('UnicornsApp' , []);
 
+//services
+require('./services/resource_service')(UnicornsApp);
+
+
+//controllers
 require('./unicorns/controllers/unicorns_controller')(UnicornsApp);
+
+//directives
